eb25d0511ffe7e6305b15847060184bc04708b3f
Rohan Patra <rohanpatra@berkeley.edu>
2024-06-23 11:21:54 -0700

    feat: header logo

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -13,6 +13,8 @@ export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/spinningplant.gif" alt="Site Logo" className="size-6" />
         <span className="inline-block font-bold">{siteConfig.name}</span>
       </Link>
       {items?.length ? (
